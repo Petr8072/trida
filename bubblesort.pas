ff64d0211aa1683f3d4458f6f82c1fd914b73d1d
Logotrop <keramdobes@gmail.com>
2016-04-14 10:28:31 +0200

    bubblesort

---
+++ bubblesort.pas
@@ -0,0 +1,22 @@
+program bubblesort_v2;
+uses Crt;
+const n=5;
+var i,j,pom : integer;
+pole : array [1..n] of integer;
+
+begin
+for i:=1 to n do begin
+writeln ('zadej ',i,'. cislo');
+readln(pole[i]);
+end;
+ for i:=n downto 2 do 
+ for j:=1 to i-1 do
+   if (pole[j+1]<pole[j]) then begin
+   pom:=pole[j+1];
+   pole[j+1]:=pole[j];
+   pole[j]:=pom;
+ end;
+ writeln;
+ for i:=1 to n do write (pole[i],',');
+ repeat until keypressed;
+end.
