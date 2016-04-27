0af71898aee67ec36bb0a09148555373558e8614
Logotrop <keramdobes@gmai.com>
2016-04-27 12:31:53 +0200

    PRG - ročníková práce
    jen já

--- PRG/rocnikovka/prg-23.pas
+++ PRG/rocnikovka/prg-23.pas
@@ -2,12 +2,34 @@ program prg_23;
 uses Crt;
 
 const n=5;
+var pole :array [1..n,1..n] of real;
+  i,j : integer;
 
-var pole :array [1..n,1..n] of real
+procedure vypis;
+begin
+  writeln;
+  for i:= 1 to n do
+  for j:=1 to n do
+  begin
+    write(pole[i,j]:4:0);
+    if (j=n) then writeln
+  end;
+end;
+procedure vyber;
+var a : string;
+begin
+  writeln ('chcete zadat cisla rucne? (y/n)');
+  read (a);
+  if (a<>'y') then for i:=1 to n do for j:=1 to n do pole[i,j]:=random (100)+1
+  else for i:=1 to n do for j:=1 to n do begin
+  writeln ('zadej ',i,'-',j,', cislo');
+  readln (pole[i,j]);
+   end;
+end;
 
 begin
-for i :=1 to n do
-  for j:=1 to n do begin
-  writeln ('zadej ',i,'. cislo');
-  end;
+randomize;
+vyber;
+vypis;
+repeat until keypressed;
 end.
