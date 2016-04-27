cf206fd28c7c73fe10d03107251b793d3226a730
Logotrop <keramdobes@gmai.com>
2016-04-27 10:51:42 +0200

    PRG - ročníková práce
    jen já

---
+++ PRG/rocnikovka/prg-23.pas
@@ -0,0 +1,13 @@
+program prg_23;
+uses Crt;
+
+const n=5;
+
+var pole :array [1..n,1..n] of real
+
+begin
+for i :=1 to n do
+  for j:=1 to n do begin
+  writeln ('zadej ',i,'. cislo');
+  end;
+end.
